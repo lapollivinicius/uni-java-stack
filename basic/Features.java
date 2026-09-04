eafafbfc624d66750bce8ffab88e65bdc590b98c
vini <229406217+lapollivinicius@users.noreply.github.com>
2026-09-04 13:30:22 -0300

    section 6

---
+++ basic/Features.java
@@ -0,0 +1,23 @@
+/**
+ * This is a documentation information
+ *
+ * @author My Name
+ * @version 1.2.0
+ * @since 1.1
+ */
+public class Features {
+
+    /**
+     * This is a doc to method
+     * @param n1 integer
+     * @param n2 integer
+     * @since 1.0
+     * @return n1 + n2
+     */
+    public int sum(int n1, int n2) {
+        return n1 + n2;
+    }
+
+    // TODO this is a task
+
+}
